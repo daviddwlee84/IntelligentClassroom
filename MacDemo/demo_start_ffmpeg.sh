905188ece8374778e493fad6e5f59f16cfa7b10a
David Lee <daviddwlee84@gmail.com>
2019-01-15 12:59:33 +0800

    Little update of ffserver config

--- MacDemo/demo_start_ffmpeg.sh
+++ MacDemo/demo_start_ffmpeg.sh
@@ -2,5 +2,5 @@ mkdir -p log
 
 nohup ./ffserver -f ../FFServer/ffserver_webcam.conf > log/ffserver_webcam.log &
 nohup ./ffserver -f ../FFServer/ffserver_screen.conf > log/ffserver_screen.log &
-nohup ./ffmpeg -f avfoundation -framerate 30 -pixel_format yuyv422 -i "0" http://localhost:8090/camera.ffm > log/ffmpeg_camera.log &
+nohup ./ffmpeg -f avfoundation -framerate 30 -pixel_format yuyv422 -i "0:0" http://localhost:8090/camera.ffm > log/ffmpeg_camera.log &
 nohup ./ffmpeg -f avfoundation -framerate 30 -pixel_format yuyv422 -i "1" http://localhost:8091/screen.ffm > log/ffmpeg_screen.log &
